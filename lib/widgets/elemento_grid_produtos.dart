39565a97d71d75df6727b91877f0c4d5763c7773
Lucas Ortigara Reis <lucas@jornadatecnologia.com>
2021-07-30 02:10:24 -0300

    Uso do elemento "Positioned"

--- lib/widgets/elemento_grid_produtos.dart
+++ lib/widgets/elemento_grid_produtos.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:lojinha_alura/modelos/movel.dart';
+import 'package:lojinha_alura/widgets/degrade_elemento_grid_produtos.dart';
 import 'package:lojinha_alura/widgets/imagem_elemento_grid_produtos.dart';
+import 'package:lojinha_alura/widgets/titulo_elemento_grid_produtos.dart';
 
 class ElementoGridProdutos extends StatelessWidget {
   final Movel? movel;
@@ -9,8 +11,15 @@ class ElementoGridProdutos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ImagemElementoGridProdutos(
-      imagem: movel!.foto ?? '',
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        ImagemElementoGridProdutos(imagem: movel!.foto ?? ''),
+        DegradeElementoGridProdutos(),
+        TituloElementoGridProdutos(
+          titulo: movel!.titulo,
+        )
+      ],
     );
   }
 }
